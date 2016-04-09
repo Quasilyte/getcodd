9d2ad15404548a33934070c141c830618d4ed876
quasilyte <quasilyte@gmail.com>
2016-04-09 03:37:19 +0300

    rosetta_code strategy -- work in progress

--- src/rosetta_strategy.rkt
+++ src/rosetta_strategy.rkt
@@ -1,10 +1,10 @@
 #lang racket
 
-(require "http.rkt")
+(require "http.rkt"
+         "strategy.rkt"
+         "storage.rkt")
 
-(require "strategy.rkt")
-
-(define fetch-anchors
+(define page-anchors
   (let ([rx-page-haystack
          #rx"<h2>Pages in category"]
         [rx-page-anchors
@@ -40,14 +40,37 @@
             [else anchors]))))
 
 (define (lang-url lang)
-  ;; not working for urls with url-encoded chars (C++ -> C%2B%2B)
+  ;; #FIXME: not working for urls with url-encoded chars (C++ -> C%2B%2B)
   (format "http://rosettacode.org/wiki/Category:~a" lang))
+
+(define (fetch-anchors lang)
+  ;; #FIXME: in high concurrent env. we need an update lock or
+  ;; second checkout after (page-anchors) call.
+  (define storage-key (string-append "rosetta_code/" lang))
+  (cond [(storage-contains? storage-key)
+         (printf "returning `~a` from cache\n" storage-key)
+         (storage-read storage-key)]
+        [else
+         (define lang-info (http-get (lang-url lang)))
+         (define anchors (page-anchors lang-info))
+         (printf "returning new `~a`\n" storage-key)
+         (storage-write storage-key anchors)
+         anchors]))
+
+(define (similar-enough? terms-synsets title-words)
+  (cond [(= (length terms-synsets) (length title-words))
+         (= (length title-words)
+            (foldl + 0.0 (map nlp-synset-similarity
+                              terms-synsets
+                              (map list title-words))))]
+        ;; #FIXME: should try harder (check for bigrams?)
+        [else #f]))
 
 (define (fetcher request)
   (match-define (list lang terms) request)
-  (define lang-info (http-get (lang-url lang)))
-  (displayln (string-length lang-info))
-  (length (fetch-anchors lang-info)))
+  (define terms-synsets (map nlp-synonyms terms))
+  (define anchors (fetch-anchors lang))
+  (length anchors))
 
 (define (evaluator result)
   result)
