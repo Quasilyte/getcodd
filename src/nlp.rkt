15abc15d80319b31769ca89632d61c428c375af1
quasilyte <quasilyte@gmail.com>
2016-04-09 03:36:46 +0300

    restructuring nlp module

--- src/nlp.rkt
+++ src/nlp.rkt
@@ -2,23 +2,31 @@
 
 (require "wn.rkt")
 
+
 (define (nlp-normalize words)
   (string-downcase words))
 
 (define nlp-synonyms wn-synonyms)
 
+(define (nlp-synset word)
+  ;; #FIXME: what to do with stop-words?
+  (nlp-synonyms (nlp-normalize word)))
+
+(define (nlp-synset-similarity a-synset b-synset)
+  (define intersection (set-intersect a-synset b-synset))
+  (if (= 0 (length intersection))
+      0.0
+      (exact->inexact 
+       (/ (length intersection)
+          (min (length a-synset) (length b-synset))))))
+
 (define (nlp-similarity a b)
-  (let ([a-synset (nlp-synonyms (nlp-normalize a))]
-        [b-synset (nlp-synonyms (nlp-normalize b))])
-    (define intersection (set-intersect a-synset b-synset))
-    (if (= 0 (length intersection))
-        0.0
-        (exact->inexact 
-         (/ (length intersection)
-            (min (length a-synset) (length b-synset)))))))
+  (nlp-synset-similarity (nlp-synset a) (nlp-synset b)))
 
 (provide nlp-normalize
          nlp-synonyms
+         nlp-synset
+         nlp-synset-similarity
          nlp-similarity)
 
 
