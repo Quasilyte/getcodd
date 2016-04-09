6a20ab489ee5b1d73cba4a721e6152d5ef4e3713
quasilyte <quasilyte@gmail.com>
2016-04-09 06:42:53 +0300

    some utilities for emacs plugin

---
+++ emacs/send_once.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+const int READ_SIZE = 1024 * 5;
+
+int main(int argc, char* argv[]) {
+  if (3 != argc) {
+    return 1;
+  }
+
+  const char* file_name = argv[1];
+  const char* query_body = argv[2];
+
+  char response_buf[READ_SIZE];
+  
+  // make request
+  int requester = open("getcodd-in", O_WRONLY);
+  if (-1 == requester) {
+    return 2;
+  }
+  write(requester, query_body, strlen(query_body));
+  close(requester);
+  
+  // wait for response
+  int responder = open(file_name, O_RDONLY);
+  if (-1 == responder) {
+    return 3;
+  }
+  read(responder, &response_buf, READ_SIZE);
+  puts(response_buf);
+
+  close(responder);
+  
+  return 0;
+}
